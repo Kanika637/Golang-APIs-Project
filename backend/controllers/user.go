f8bec0ec296422a80d7131311e43af3a4ae04657
Kanika Gola <kanikagola26@gmail.com>
2023-03-16 19:22:42 +0530

    Connection with database was successfull

---
+++ backend/controllers/user.go
@@ -0,0 +1,87 @@
+package controllers
+
+import(
+"fmt"
+"encoding/json"
+"github.com/julienschmidt/httprouter"
+"gopkg.in/mgo.v2"
+"gopkg.in/mgo.v2/bson"
+"github.com/HousewareHQ/backend-engineering-octernship/models"
+"net/http"
+)
+
+type UserController struct{
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController{
+return &UserController{s}
+}
+
+func (uc UserController) GetUser (w http.ResponseWriter, r *http.Request, p httprouter.Params){
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id){
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	u := models.User{}
+
+  if err := uc.session.DB("HousewareHQ").C("Users").FindId(oid).One(&u); err != nil{
+	w.WriteHeader(404)
+	return
+   }
+
+   uj, err :=json.Marshal(u)
+   if err!= nil{
+	   fmt.Println(err)
+   }
+
+w.Header().Set("Content-Type", "application/json")
+w.WriteHeader(http.StatusOK)
+fmt.Fprintf(w, "%s\n", uj)
+
+}
+
+
+func (uc UserController) CreateUser (w http.ResponseWriter, r *http.Request, _ httprouter.Params){
+	u := models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	u.Id = bson.NewObjectId()
+
+	uc.session.DB("HousewareHQ").C("Users").Insert(u)
+
+	uj, err := json.Marshal(u)
+
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params){
+
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id){
+		w.WriteHeader(404)
+		return
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	if err := uc.session.DB("Houseware").C("Users").RemoveId(oid); err != nil {
+		w.WriteHeader(404)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Deleted user", oid, "\n")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kanika/houseware---backend-engineering-octernship-Kanika637
+
+go 1.20
